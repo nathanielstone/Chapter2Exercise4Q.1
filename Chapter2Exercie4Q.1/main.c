ab66a8d841efe950bcabfcd7ffd656b68fcd92b7
Nathaniel <nathan@stone.tc>
2016-02-15 11:55:14 -0600

    Trying to make my first commit

--- Chapter2Exercie4Q.1/main.c
+++ Chapter2Exercie4Q.1/main.c
@@ -7,9 +7,38 @@
 //
 
 #include <stdio.h>
+#define MAX  5
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int arr[5][5];
+    int i,j,k = 0, p = 0;
+    int row = 5;
+    int col = 5;
+    
+    
+    for (i = 0; i < row; i++) {
+        for (j = 0; j < col; j++) {
+            arr[i][j] = k;
+            k++;
+            printf(" %d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
+    
+    
+    for (i = 0; i < row; i++) {
+        for (j = 0; j < col; j++) {
+            arr[i][j] = p;
+            p++;
+            
+            printf(" %d ", arr[j][i]);
+            
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
